cc96abc435759c041ae1436973a9b6f19bb22961
junho85 <junho85@gmail.com>
2016-04-04 21:57:04 +0900

    add footer and header

---
+++ application/views/footer.php
@@ -0,0 +1,14 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 길죽한 텍스트 광고 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-8756229796915320"
+     data-ad-slot="3176644578"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+</div><!-- container -->
+
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Junho Labs with CodeIgniter</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+
+	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<style type="text/css">
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+
+
+	</style>
+</head>
+<body>
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="navbar-inner">
+	<div class="container">
+	  <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="brand" href="<?=$this->config->item("base_url")?>">JunHo's Lab with CI</a>
+	  <div class="nav-collapse collapse">
+		<ul class="nav">
+		  <!--
+		  <li class="active"><a href="#">Home</a></li>
+		  <li><a href="#about">About</a></li>
+		  <li><a href="#contact">Contact</a></li>
+		  -->
+		</ul>
+	  </div><!--/.nav-collapse -->
+	</div>
+  </div>
+</div>
+
+<div class="container">
